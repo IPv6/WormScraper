59949c9ae7108d722a538e568516c7de6b44f425
Ilja Razinkov <razinkov@gmail.com>
2025-04-07 19:52:53 +0300

    Add files via upload

---
+++ sitemap/pale.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import requests
+import os.path
+import sys
+
+# I switched around to the requests module on this one because I was told it yields better results.
+
+# Also unlike worm or pact there was no table of contents so I had to scrape from the sitemap.
+req = requests.get("https://palewebserial.wordpress.com/sitemap.xml")
+xml = req.text
+soup = BeautifulSoup(xml,"lxml")
+
+# Finds all url tags within the xml file.
+sitemapTags = soup.find_all("url")
+print("sitemapTags", sitemapTags)
+
+dictionary = {}
+i = 0
+
+# Opens the output sitemap file.
+if(os.path.isfile("pale-sitemap.txt")):
+    print("Sitemap already exists, either from a previous scrape, or user has generated a custom sitemap.\n")
+    print("Not generating sitemap.\n")
+    sys.exit(0)
+else:
+    file = open("pale-sitemap.txt","w")
+    print("File opened\n")
+
+# Finds every url in the sitemap that hold 2014/15/16/17/18 since that's the naming convention followed,
+# and adds it to a dictionary.
+for sitemap in sitemapTags:
+    current = sitemap.findNext("loc").text
+    Keywords = ['2014','2015','2016','2017','2018','2020','2021','2022','2023']
+    if any(keys in current for keys in Keywords):
+        dictionary[i] = current
+        i+=1
+
+# The sitemap holds the urls in reverse chronological order so I can to reverse the entire URL list.
+while i > 0:
+    print("Getting link : " + dictionary[i-1] + "\n" )
+
+# Writing URL's in chronological order to the file.
+    file.write(dictionary[i-1])
+    file.write("\n")
+    i-=1
+
+print("All done! URL file generated. \n")
+file.close()
