9f9994a07612f77f80847fe3127d95eafa2a29b6
Ilja Razinkov <razinkov@gmail.com>
2025-04-20 02:37:32 +0300

    Update pact.py

--- sitemap/pact.py
+++ sitemap/pact.py
@@ -12,37 +12,39 @@ source = urllib.request.urlopen('https://pactwebserial.wordpress.com/table-of-co
 # Creates required object.
 soup = bs.BeautifulSoup(source,'lxml')
 
+
+sm_file = sys.argv[1]
 # Opens a text file and writes all required URL's into it.
-if(os.path.isfile("pact-sitemap.txt")):
-    print("Sitemap already exists, either from a previous scrape, or a custom sitemap.")
-    print("Not generating sitemap.")
-    sys.exit(0)
+if(os.path.isfile(sm_file)):
+	print("Sitemap already exists, using it.")
+	sys.exit(0)
 else:
-    file = open("pact-sitemap.txt","w")
-    print("File opened\n")
+	file = open(sm_file,"w")
+	print("Creating Sitemap using Table-of-contents.")
+	print("WARNING: Table-of-contents CONTAIN ERRORS. Custom fix needed for some chapters (7,8,10,15,16)")
 
-# Finds all anchor tags and gets the links they point to.
-for url in soup.find_all('a'):
-    current = url.get('href')
+	# Finds all anchor tags and gets the links they point to.
+	for url in soup.find_all('a'):
+		current = url.get('href')
 
-    # The URL at which we need to stop scraping.
-    lastURL = "https://pactwebserial.wordpress.com/2015/02/28/judgment-16-12/"
-    # We do not need any URL's after this one, since this is the end of the serial.
-    if(current==lastURL):
-        file.write(lastURL)
-        print("All done! URL file generated\n")
-        break
+		# The URL at which we need to stop scraping.
+		lastURL = "https://pactwebserial.wordpress.com/2015/02/28/judgment-16-12/"
+		# We do not need any URL's after this one, since this is the end of the serial.
+		if(current==lastURL):
+			file.write(lastURL)
+			print("All done! URL file generated\n")
+			break
 
-    # The URL's vary a lot with title so I had to get creative on how to get only those we needed. Not the best way but it works.
-    Keywords = ['2013','2014','2015']
-    if any(keys in current for keys in Keywords):
-        if "https://" not in current:
-            file.write("https://" + current + "\n")
-        else:
-            # broken links in Table of content WTF
-            if current == "https://pactwebserial.wordpress.com/2014/06/10/void-7-5/":
-                file.write("https://pactwebserial.wordpress.com/2014/06/03/void-7-3/" + "\n")
-                file.write("https://pactwebserial.wordpress.com/2014/06/07/void-7-4/" + "\n")
-            file.write(current + "\n")
-            print("Getting link " + url.get('href') + "\n")
+		# The URL's vary a lot with title so I had to get creative on how to get only those we needed. Not the best way but it works.
+		Keywords = ['2013','2014','2015']
+		if any(keys in current for keys in Keywords):
+			if "https://" not in current:
+				file.write("https://" + current + "\n")
+			else:
+				# broken links in Table of content WTF - many of them... monkey-patching futile
+				#if current == "https://pactwebserial.wordpress.com/2014/06/10/void-7-5/":
+				#	file.write("https://pactwebserial.wordpress.com/2014/06/03/void-7-3/" + "\n")
+				#	file.write("https://pactwebserial.wordpress.com/2014/06/07/void-7-4/" + "\n")
+				file.write(current + "\n")
+				print("Getting link " + url.get('href') + "\n")
 file.close()
